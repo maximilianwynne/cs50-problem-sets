9609c53568b6fbf8b71ca247f81966b161d8fcc8
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-26 23:20:18 +0000

    Update substitution.c

--- substitution.c
+++ substitution.c
@@ -6,39 +6,47 @@
 
 int main(int argc, string argv[])
 {
-
+    // check number of command line arguments
     if (argc != 2)
     {
         printf("Nope\n");
         return 1;
     }
-    if (strlen(argv[1]) != 26)
+
+    string key = argv[1];
+
+    // check key length
+    if (strlen(key) != 26)
     {
         printf("Error");
         return 1;
     }
-    for (int i = 0; i < strlen(argv[1]); ++i)
+
+    // check for non-letters in key.
+    for (int i = 0; i < strlen(key); ++i)
     {
-        if (!(islower(argv[1][i]) || isupper(argv[1][i])))
+        if (!(islower(key[i]) || isupper(key[i])))
 			return 1;
     }
-    char* c;
 
-    int n = strlen(argv[1]);
+    char c;
 
+    int n = strlen(key);
+
+    // check for duplicate letters in key
     for (int i = 0; i < n; ++i)
     {
-    c = argv[i];
+        c = key[i];
 
-    	for(int j = 0; j < n; ++j)
+    	for(int j = i + 1; j < n; ++j)
     	{
-    		if (c == argv[j])
+    		if (c == key[j])
     		{
     			return 1;
     		}
     	}
     }
-    string key = argv[1];
+
 
     string code = get_string("plaintext: ");
 
@@ -47,14 +55,14 @@ int main(int argc, string argv[])
         if (islower(code[i]))
         {
             int pos = code[i] - 'a';
-			code[i] = argv[1][pos];
+			code[i] = key[pos];
 			if (isupper(code[i]))
 				code[i] += 32;
         }
         else if (isupper(code[i]))
         {
             int pos = code[i] - 'A';
-            code[i] = argv[1][pos];
+            code[i] = key[pos];
 			if (islower(code[i]))
 				code[i] -= 32;
         }
