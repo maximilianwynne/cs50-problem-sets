f9472c00cc1836107364600f76eb488fbf0c73b3
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-04-09 16:48:23 +0100

    Update filters(more).c

--- filters(more).c
+++ filters(more).c
@@ -1,5 +1,28 @@
 #include "helpers.h"
 
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop through rows
+    for (int i = 0; i < height; i++)
+    {
+        // Loop through columns
+        for (int j = 0; j < width; j++)
+        {
+            // Get RGBTriple values
+            float blue = image[i][j].rgbtBlue;
+            float green = image[i][j].rgbtGreen;
+            float red = image[i][j].rgbtRed;
+
+            // Calculate average
+            int average = round((image[j][i].rgbtBlue + image[j][i].rgbtGreen + image[j][i].rgbtRed) / 3);
+
+            // Set all values to average
+            image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i]  [j].rgbtRed = average;
+        }
+    }
+    return;
+}
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
