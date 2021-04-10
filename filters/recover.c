714b1ec2cb43385aa0378d38001fff101b06b857
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-04-10 22:20:32 +0100

    Create recover.c

---
+++ filters/recover.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *f = fopen(filename, "r");
+    fread(filename, "r");
+
+    if (argc = 0xd8 || 0xff || 0xef
+    {
+        printf()
+    }
+}
