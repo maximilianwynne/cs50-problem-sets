33f9748c1cfb901297ac2d17ecb43da215c58120
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-04-11 23:38:19 +0100

    Update recover.c

--- filters/recover.c
+++ filters/recover.c
@@ -1,31 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// find first JPEG block and open it
+// find first JPEG block and open it on the memory card
 int main(int argc, char* argv[])
 {
     char const* const fileName = argv[1];
-    FILE* file = fopen(fileName, "r");
+    FILE* f = fopen(filename, "r");
     char line[1];
 
-    while (fgets(line, sizeof(line), file))
+    while(fgets(line, sizeof(line), file))
     {
         printf("%s", line);
     }
-
-    fclose(file);
-
-    return 0;
+    else
+    {
+    while (fscanf(f, "%s", &etc) != EOF)
+        {
+            f = fgetc( fp );
+            if( feof(fp) )
+            break ;
+            printf( "%c", c);
+        }
+    }
 }
 
-fread(data, size, number, input);
+// check if JPEG file is open
+int main(int argc, char* argv[])
+{
+    if (fp != NULL)
+    {
+        fclose (fp);
+        fp = NULL;
+    }
+}
 
-    if (buffer = 0xff || 0xd88 || 0xff)
+while (fread fileName) {
+
+    if (buffer = 0xff || 0xd88 || 0xff) {
     {
         buffer[0] == 0xff
-        buffer[1] == 0xd88
+        buffer[1] == 0xd8
         buffer[2] == 0xff
-        buffer[3] & 0xf0 == 0xe0
-    }
+        (buffer[3] & 0xf0) == 0xe0
 
-sprintf(filename, "%03i.jpg", 2);
+        sprintf(int argc, char* argv[1]; i++)
+        printf(fileName);
+    }
+}
+
+// check if JPEG file is open
+int main(int argc, char* argv[]) {
+{
+    if (fp != NULL)
+    {
+        fclose (fp);
+        fp = NULL;
+    }
+}
+
+// Write the block to JPEG file (or append)
+
+strncat(block, &ch, 1);
