2869e0531e3f28fe936133fa216815fae203ebef
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-04-12 23:09:54 +0100

    Update recover.c

--- filters/recover.c
+++ filters/recover.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+FILE * image = NULL
+
 // find first JPEG block and open it on the memory card
 int main(int argc, char* argv[])
 {
+    // open the file
+    FILE* in_p = fopen("card.raw", "r");
+
+    // check if file exists and can be opened
     char const* const fileName = argv[1];
     FILE* f = fopen(filename, "r");
     char line[1];
@@ -24,19 +30,13 @@ int main(int argc, char* argv[])
     }
 }
 
-// check if JPEG file is open
-int main(int argc, char* argv[])
+// declare the buffer where the block of 512 bytes will be stored
+unsigned buffer storage[512];
+
+// read from the file until it ends
+while (fread fileName)
 {
-    if (fp != NULL)
-    {
-        fclose (fp);
-        fp = NULL;
-    }
-}
-
-while (fread fileName) {
-
-    if (buffer = 0xff || 0xd88 || 0xff) {
+    if (buffer = 0xff || 0xd88 || 0xff)
     {
         buffer[0] == 0xff
         buffer[1] == 0xd8
@@ -48,16 +48,53 @@ while (fread fileName) {
     }
 }
 
-// check if JPEG file is open
-int main(int argc, char* argv[]) {
+// read from the file until it ends
+while (fread(&buffer, sizeof(buffer), 1, in_p))
 {
-    if (fp != NULL)
+    //const char mark_1[4] =
+    if (buffer[0] == 0xff &&
+        buffer[1] == 0xd8 &&
+        buffer[2] == 0xff &&
+        (buffer[3] == 0xe0 || buffer[3] == 0xe1))
+
+
     {
-        fclose (fp);
-        fp = NULL;
+    // if the new jpeg sequence is found, write into new JPEG until a new JPEG sequence is found
+    printf("found \n ");
+    break;
+    // FILE* out_p = fopen("title", "a");
+    // fwrite(&buffer, sizeof(buffer), 1, out_p);
+
+
+
+    //fclose(out_p);
+    }
+
+    else
+    {
+        printf("not found \n");
+    }
+
+// Write the block to JPEG file (or append)
+unsigned char buffer[3];
+FILE *ptr;
+{
+    ptr = fopen("card.raw", "rb");
+    fread(buffer,sizeof(buffer),1,ptr);
+
+    for(int i = 0; i < 10; i++)
+}
+    {
+        printf("%u ", buffer[i]);
     }
 }
 
-// Write the block to JPEG file (or append)
-
-strncat(block, &ch, 1);
+FILE *write_ptr;
+{
+    write_ptr = fopen("card.raw", "wb");
+    fwrite(buffer, sizeof(buffer), 1, write_ptr);
+    {
+        // close the JPEG file
+        fclose(in_p);
+    }
+}
