7ff8ae00b8b165b8761ffe3a89992960fb1b2445
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-05-09 00:19:03 +0100

    Update helpers.c

--- filters/helpers.c
+++ filters/helpers.c
@@ -45,7 +45,24 @@ int limit(int RGB)
 
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    
+    int sepiaBlue;
+    int sepiaRed;
+    int sepiaGreen;
+    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            sepiaBlue = limit(round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue));
+            sepiaGreen = limit(round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue));
+            sepiaRed = limit(round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue));
 
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtRed = sepiaRed;
+        }
+    }
 }
 
 // Reflect image horizontally
