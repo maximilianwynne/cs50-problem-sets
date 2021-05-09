99d589b37e8d4872aa80610d2881161d11d7821e
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-05-09 17:55:31 +0100

    Update helpers.c

--- filters/helpers.c
+++ filters/helpers.c
@@ -85,34 +85,58 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
-// Blurring image
+// Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Create temp array to duplicate the image into the array.
-    // You will need the original pixels to calculate the averages (stored in the array), which will be removed from the original image once we start blurring
+    // create temp array to store duplicated image before blurring
     RGBTRIPLE temp[height][width];
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-             temp[i][j] = image[i][j];
+            temp[i][j] = image[i][j];
         }
     }
-    // Loop through rows
+    // loop through each pixel and declare variable to store RGB values + count (counter) the amount of pixels around middle pixel
     for (int i = 0; i < height; i++)
     {
-        // Loop through columns
         for (int j = 0; j < width; j++)
         {
-            // Initialise values
-            // Variable declared to store the sums of red, blue and green values of the surrounding pixels, and a counter of the amount of surrounding pixels
+            // initialise values
             float sum_red;
             float sum_blue;
             float sum_green;
-            // counter for the surrounding pixels
             int counter;
 
+            // if height or width outside of 0, then pixel doesn't exist
             sum_red = sum_blue = sum_green = counter = 0;
+
+            for (int k = -1; k < 2; k++)
+            {
+                for (int l = -1; l < 2; l++)
+                {
+                    // check if pixel is outside rows
+                    if (i + k < 0 || i + k >= height)
+                    {
+                        continue;
+                    }
+                    // check if pixel is outside columns
+                    if (j + l < 0 || j + l >= width)
+                    {
+                        continue;
+                    }
+                    // add to each colour's sum and increment the counter when the pixels exist
+                    sum_red += temp[i + k][j + l].rgbtRed;
+                    sum_blue += temp[i + k][j + l].rgbtBlue;
+                    sum_green += temp[i + k][j + l].rgbtGreen;
+                    counter++;
+                }
+            }
+            // get average for each pixel and round up to then blur image
+            image[i][j].rgbtRed = round(sum_red / counter);
+            image[i][j].rgbtGreen = round(sum_green / counter);
+            image[i][j].rgbtBlue = round(sum_blue / counter);
         }
     }
 }
