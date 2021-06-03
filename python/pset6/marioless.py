f3f1eb0fd5808e345576ed8775aa74a3055ac69f
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-06-03 21:51:09 +0100

    Create marioless.py

---
+++ python/pset6/marioless.py
@@ -0,0 +1,16 @@
+def get_value():
+    a = 0
+    while a < 1 or a > 8:
+        a = int(input("Height: "))
+    return a
+
+
+def draw(height):
+    for row in range(height):
+        num_spaces = height - row - 1
+        num_hashes = row + 1
+        print(" " * num_spaces + "#" * num_hashes)
+
+
+height = get_value()
+draw(height)
