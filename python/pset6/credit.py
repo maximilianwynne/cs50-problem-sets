d42c39de5f1907c1c50ba364ed99928d32d94336
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-06-19 23:23:23 +0100

    Create credit.py

---
+++ python/pset6/credit.py
@@ -0,0 +1,13 @@
+i = float(input("Enter your credit card number: "))
+
+if i > 16 or i < 13:
+    print("INVALID\n")
+
+if i == 13 or 16:
+    print("VISA\n")
+
+if i == 15:
+    print("AMEX\n")
+
+if i == 16:
+    print("MASTERCARD\n")
