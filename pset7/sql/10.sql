aba9b6fc2ac9a461e433d7d6a6e0a6f58c293706
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-07-09 17:23:45 +0100

    Update 10.sql

--- pset7/sql/10.sql
+++ pset7/sql/10.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT(people.name) from stars
-JOIN people ON stars.person_id = people.id
+SELECT DISTINCT(people.name) from directors
+JOIN directors ON directors.person_id = directors.id
 JOIN movies ON stars.movie_id = movies.id
-WHERE movies.year = 2004
-ORDER BY birth;
+WHERE movies.score <= 9.0
+ORDER BY movies.score;
