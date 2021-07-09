78865f9ce2f09e208fe741ffa31b86cc46e7acb7
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-07-09 17:33:38 +0100

    Update 10.sql

--- pset7/sql/10.sql
+++ pset7/sql/10.sql
@@ -1,6 +1,6 @@
-SELECT DISTINCT(people.name) from directors
-JOIN directors ON directors.id = directors.person_id
+SELECT DISTINCT(name) from people
+JOIN directors ON people.id = directors.person_id
 JOIN movies ON directors.movie_id = movies.id
 JOIN ratings ON movies.id = ratings.movie_id
-WHERE movies.score => 9.0
-ORDER BY movies.score;
+WHERE ratings.movie => 9.0
+ORDER BY ratings.movie;
