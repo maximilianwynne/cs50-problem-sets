91d7202eb0dc6e5791e932189ea21b5bf288e699
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-07-09 17:19:30 +0100

    Update 8.sql

--- pset7/sql/8.sql
+++ pset7/sql/8.sql
@@ -1,4 +1,4 @@
-SELECT people.name
-FROM movies
-JOIN people.name ON stars = names
-WHERE names IN movie = 'Toy Story';
+SELECT name from people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE title IN movie = 'Toy Story';
