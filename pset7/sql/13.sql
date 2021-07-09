b77c9371c6147a9a635a3a3f19afca1a578df46b
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-07-09 17:40:55 +0100

    Update 13.sql

--- pset7/sql/13.sql
+++ pset7/sql/13.sql
@@ -1,6 +1,9 @@
-SELECT movies.title
+SELECT distinct(name)
 FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-GROUP BY 'Kevin Bacon' IN '1958' IN movies.title
-DELETE FROM people WHERE people = "Kevin Bacon";
+WHERE movies.title IN(SELECT DISTINCT(movies.title) FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958) AND people.name != "Kevin Bacon";
+DELETE FROM people WHERE people.name = "Kevin Bacon";
