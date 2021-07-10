e8dc4384603060c991eb4edae09769fe46bc8cde
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-07-10 21:31:10 +0100

    Create movies.sql

---
+++ pset7/sql/movies.sql
@@ -0,0 +1,10 @@
+from cs50 import SQL
+db = SQL("sqlite:///students.db")
+    input_file = csv.DictReader(open("characters.csv"))
+    reader = csv.DictReader(characters.csv)
+    for column in reader
+        print(column['name'], column['house'], column['birth'])
+
+db.execute("QUERY")
+
+print(row)
