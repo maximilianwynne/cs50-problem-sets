1d41f423bd69e6653fc072af2286dcdd6b3b0f41
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-07-20 21:35:14 +0100

    Update import.py

--- pset7/sql/import.py
+++ pset7/sql/import.py
@@ -20,11 +20,11 @@ with open(sys.argv[1], "r") as characters:
         if len(name_list) == 2:
             first_name = name_list[0]
             last_name = name_list[1]
-            db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)"
+            db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
                        first_name, None, last_name, row["house"], row["birth"])
         elif len(name_list) == 3:
             first_name = name_list[0]
             middle_name = name_list[1]
             last_name = name_list[2]
-            db.execute("INSERT INTO students (first, midle, last, house, birth) VALUES(?, ?, ?, ?, ?)"
+            db.execute("INSERT INTO students (first, midle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
                        first_name, middle_name, last_name, row["house"], row["birth])    
