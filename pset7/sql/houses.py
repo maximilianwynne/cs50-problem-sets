5701cc0b2662a98b76970b7b9836506628cf2968
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-07-20 21:31:28 +0100

    Update houses.py

--- pset7/sql/houses.py
+++ pset7/sql/houses.py
@@ -1,35 +1,30 @@
 from cs50 import SQL
-from csv import reader
-from sys import argv
+from csv
+from sys
 
+if len(sys.argv) != 2:
+    print("Python import.py characters.csv incorrect")
+    sys.exit(1)
+    
 db = SQL("sqlite:///students.db")
-    input_file = csv.DictReader(open("characters.csv"))
-    reader = csv.DictReader(characters.csv)
-    for column in reader
-        print(column['name'], column['house'], column['birth'])
 
-# check to see if we have the correct number of command line arguments
-if len(argv) < 2:
-    print("incorrect number of command line arguments; provide name of CSV file")
-    exit()
-
-# open CSV file and read line-by-line
-open(argv[1], newline='') as charactersFile("characters.csv"):
-    characters = reader(charactersFile)
-    for character in characters:
-        if character[0] == 'name':
-            continue
-
-        # print first, middle (if there is middle) and last names for each student
-        studentName = name.first, name.middle, name.last
-        studentName = character.split()
-        if len(studentName) < 3:
-            db.execute("insert into students.db(first, middle, last, house, birth")
-            VALUES = name.first(?), name.middle(?), name.last(?), house(?), birth(?)
-            name.first[0], name.middle[None], name.last[1], character[1], character[2]
-
-        else:
-            db.execute("insert into students.db9first, middle, last, house, birth")
-            name.first[0], name.middle[1], name.last[1], character[1], character[2]
-
-print(row)
+with open(sys.argv[1], "r") as characters:
+    
+    reader = csv.DictReader(Characters, Delimiter=",")
+    
+    for row in reader:
+        
+        name = row["name"]
+        name_list = name.split()
+        
+        if len(name_list) == 2:
+            first_name = name_list[0]
+            last_name = name_list[1]
+            db.execute("INSERTT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)
+                       first_name, None, last_name, row["house"], row["birth"])
+        elif len(name_list) == 3:
+            first_name = name_list[0]
+            middle_name = name_list[1]
+            last_name = name_list[2]
+            db.execute("INSERT INTO students (first, midle, last, house, birth) VALUES(?, ?, ?, ?, ?)
+                       first_name, middle_name, last_name, row["house"], row["birth])    
