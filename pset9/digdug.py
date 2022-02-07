57552ab71ddc7a725a7cc9b25a74041223ec9101
maxwynne <76392789+maxwynne@users.noreply.github.com>
2022-02-07 23:16:40 +0000

    Update digdug.py

--- pset9/digdug.py
+++ pset9/digdug.py
@@ -1,75 +1,15 @@
 import pygame
+import os
 
-width = 800
-height = 600
+WIDTH = 800
+HEIGHT = 600
 
 background_colour = (234, 212, 252)
 
 pygame.init()
-width, height = 640, 480
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-pygame.display.set_caption('Mr Dig')
-
-screen.fill(background_colour)
-pygame.display.flip()
-running = True
-
-# game loop / keep live
-while running:
-    # loop through event queue
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-# set blank screen
-def main():
-    pygame.display.init()
-    screen = pygame.display.set_mode((800, 600))
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.images = ['hero.png']
-
-        charimage = pygame.image.load(os.path.join(os.path.sep, 'hero.png')).convert()
-
-def move():
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT or event.key == ord('a'):
-            print('left')
-        if event.key == pygame.K_RIGHT or event.key == ord('d'):
-            print('right')
-        if event.key == pygame.K_UP or event.key == ord('w'):
-            print('jump')
-
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT or event.key == ord('a'):
-            print('left stop')
-        if event.key == pygame.K_RIGHT or event.key == ord('d'):
-            print('right stop')
-        if event.key == ord('q' or 'e'):
-            pygame.quit()
-            main = False
-
-class Hero(Character)
-    def __init__(self):
-        Character.__init__(self, "Hero")
-        self.hurt = pygame.image.load(os.path.join("Assets", "Hero", "digdug_hurt.png")).convert_alpha()
-        self.rect = self.surface.get_rect(centre = (WIDTH/2, HEIGHT/2))
-        self.xspeed = BASE_SPEED
-        self.yspeed = BASE_SPEED
-        self.health = BASE_HEALTH
-
-# when character moves
-    def move(self):
-        # see if keys are being pressed
-        pressed_keys = pygame.key.get_pressed()
-
-        if self.rect.left > 0 and pressed_keys[K_LEFT]:
-            self.rect.move_ip(-BASE_SPEED, 0):
-
-# draw character frame
+pygame.display.set_caption('Dig Dug')
 
 # create character / test walks
 # i need the actual pngs
@@ -77,13 +17,58 @@ class Character(pygame.sprite.Sprite):
     def __init__(self, type):
         super().__init__()
         self.walk_animation = [
-            pygame.game.load(os.path.join(type, f"{type}_standing.png")).convert_alpha(),
-            pygame.game.load(os.path.join(type, f"{type}_L.png")).convert_alpha(),
-            pygame.game.load(os.path.join(type, f"{type}_standing.png")).convert_alpha(),
-            pygame.game.load(os.path.join(type, f"{type}_R.png")).convert_alpha(),
+            pygame.image.load(os.path.join("images", f"{type}_standing.png")).convert_alpha(),
+            # pygame.game.load(os.path.join(type, f"{type}_L.png")).convert_alpha(),
+            # pygame.game.load(os.path.join(type, f"{type}_standing.png")).convert_alpha(),
+            # pygame.game.load(os.path.join(type, f"{type}_R.png")).convert_alpha(),
         ]
-        self.surface = pygame.Surface(100, 150)
         self.direction = 1 #1 is 'right', 0 is 'left'
         self.step_count = 0
 
-#
+    def update(self):
+        self.image = self.walk_animation[0]
+
+class Player(Character):
+    def __init__(self):
+        Character.__init__(self, "player")
+        # self.hurt = pygame.image.load(os.path.join("Assets", "Hero", "digdug_still.png")).convert_alpha()
+        self.rect = pygame.rect.Rect(100, 100, 50, 80)
+        self.speed = 5
+
+    def update(self):
+        # see if keys are being pressed
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        super().update()
+
+def draw():
+    screen.fill(background_colour)
+    sprites.draw(screen)
+    pygame.display.flip()
+
+sprites = pygame.sprite.Group()
+sprites.add(Player())
+
+clock = pygame.time.Clock()
+
+running = True
+
+# game loop / keep live
+while running:
+    clock.tick(60)
+
+    # loop through event queue
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    sprites.update()
+
+    draw()
