dba230687ccb690afcd4e9c48d424df2c893b811
maxwynne <76392789+maxwynne@users.noreply.github.com>
2022-03-20 19:31:49 +0000

    Update digdug.py

--- pset9/digdug.py
+++ pset9/digdug.py
@@ -13,6 +13,7 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption('Dig Dug')
+clock = pygame.time.Clock()
 
 # create character / test walks
 # i need the actual pngs
@@ -25,32 +26,46 @@ class Character(pygame.sprite.Sprite):
             # pygame.game.load(os.path.join(type, f"{type}_standing.png")).convert_alpha(),
             # pygame.game.load(os.path.join(type, f"{type}_R.png")).convert_alpha(),
         ]
-        self.direction = 1 #1 is 'right', 0 is 'left'
+        self.direction = -1 #1 is 'right', 0 is 'left'
         self.step_count = 0
+        self.image = self.walk_animation[0]
 
     def update(self):
-        self.image = self.walk_animation[0]
+        # self.image = self.walk_animation[0]
+        pass
 
 class Player(Character):
     def __init__(self):
         Character.__init__(self, "player")
         # self.hurt = pygame.image.load(os.path.join("Assets", "Hero", "digdug_still.png")).convert_alpha()
-        self.rect = pygame.rect.Rect(100, 100, 50, 80)
-        self.speed = 5
+        self.speed = 1
+        # resize character
+        scaled_image = pygame.Surface([level.tile_size, level.tile_size])
+        pygame.transform.scale(self.image, (level.tile_size,level.tile_size), scaled_image)
+        self.image = scaled_image
+        self.rect = self.image.get_rect()
 
     def update(self):
         # see if keys are being pressed
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            self.rect.y -= self.speed
-            backgroundcoord = 1
-        if keys[pygame.K_DOWN]:
-            self.rect.y += self.speed
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += self.speed
+        # check alignment with tile
+        input_x = 0
+        if keys[pygame.K_UP] or keys[pygame.K_DOWN]:
+            input_x = 1 if keys[pygame.K_DOWN] else -1
+        input_y = 0
+        if keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
+            input_y = 1 if keys[pygame.K_RIGHT] else -1
+        if self.rect.x % level.tile_size == 0:
+            # pressing down, input y = 1 - add 1 * 1 = speed
+            self.rect.y += input_y * self.speed
+        if self.rect.y % level.tile_size == 0:
+            if keys[pygame.K_LEFT]:
+                self.rect.x -= self.speed
+            if keys[pygame.K_RIGHT]:
+                self.rect.x += self.speed
         super().update()
+
+
 
 
 # implementing tiles
@@ -66,10 +81,11 @@ class Tile(pygame.sprite.Sprite):
     def draw(self, surface):
         surface.blit(self.image, (self.rect.x, self.rect.y))
 
+
 class TileMap():
     def __init__(self, filename):
         # initialise tile size
-        self.tile_size = 16
+        self.tile_size = 32
         # self.map_w, self.map_h = x * self.tile_size, y * self.tile_size
         self.start_x, self.start_y = 0, 0
         self.tile_img = pygame.image.load("tile.png")
@@ -82,6 +98,7 @@ class TileMap():
                 tile.draw(screen)
 
     def load(self, filename):
+        tile_size_original = 16
         map = []
         with open(os.path.join(filename)) as file:
             data = csv.reader(file, delimiter=',')
@@ -93,17 +110,20 @@ class TileMap():
                     number = int(number)
                     if number >= 0:
                         # get image corresponding from tile
-                        x = self.tile_size * (number % 6)
-                        y = self.tile_size * (number // 6)
-                        tile_surface = pygame.surface.Surface((self.tile_size, self.tile_size))
-                        tile_surface.blit(self.tile_img, (0, 0), Rect(x,y,self.tile_size,self.tile_size))
-                        tile_row.append(Tile(tile_surface, screen_x, screen_y))
+                        x = tile_size_original * (number % 6)
+                        y = tile_size_original * (number // 6)
+                        tile_surface_original = pygame.surface.Surface((tile_size_original,tile_size_original))
+                        tile_surface_scaled = pygame.surface.Surface((self.tile_size, self.tile_size))
+                        tile_surface_original.blit(self.tile_img, (0, 0), Rect(x,y,tile_size_original,tile_size_original))
+                        pygame.transform.scale(tile_surface_original, (self.tile_size,self.tile_size),tile_surface_scaled)
+                        tile_row.append(Tile(tile_surface_scaled, screen_x, screen_y))
                     screen_x += self.tile_size
                 map.append(tile_row)
                 screen_y += self.tile_size
         return map
 
 level = TileMap("map.csv")
+
 
 def draw():
     screen.fill(background_colour)
